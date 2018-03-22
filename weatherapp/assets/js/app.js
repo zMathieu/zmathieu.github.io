3f85c57ccb042300248cd09bbfe0e70bcf93298d
Mathieu L <mathieulux@gmail.com>
2018-03-22 17:24:42 +0100

    Remove useless header

--- weatherapp/assets/js/app.js
+++ weatherapp/assets/js/app.js
@@ -99,10 +99,6 @@ $("#geolocation").click(function (event) {
 function getMeteoByCity(city, callback){
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=" + API_KEY,
-        type: 'GET',
-        headers: {
-            "x-requested-with": "xhr"
-        },
         success: function(data){
             callback(data, null);
         },
@@ -115,10 +111,6 @@ function getMeteoByCity(city, callback){
 function getMeteoByCoordinates(lat, lon, callback){
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&APPID=" + API_KEY,
-        type: 'GET',
-        headers: {
-            "x-requested-with": "xhr"
-        },
         success: function(data){
             callback(data, null);
         },
